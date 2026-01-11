d6f3fda6fef76a84547915a0f64d0ca4e63fdac7
gufernandess <gustavofernandescc@gmail.com>
2026-01-11 13:49:25 -0300

    feat: changing members section

--- src/components/landing/Members.jsx
+++ src/components/landing/Members.jsx
@@ -1,49 +1,30 @@
 import SectionTitle from "../common/SectionTitle";
-import MemberCard from "../ui/MemberCard";
-import matheusPic from "../../assets/images/matheus.jpeg";
-import flavioPic from "../../assets/images/flavio.jpeg";
-import addnyPic from "../../assets/images/addny.jpeg";
-import thaisPic from "../../assets/images/thais.jpeg";
-import victorPic from "../../assets/images/victor.jpeg";
+import "./Members.css";
 
-const members = [
-  {
-    name: "MATHEUS",
-    instrument: "Vocal",
-    img: matheusPic,
-  },
-  {
-    name: "FLÁVIO",
-    instrument: "Guitarra",
-    img: flavioPic,
-  },
-  {
-    name: "ADDNY",
-    instrument: "Baixo",
-    img: addnyPic,
-  },
-  { name: "VICTOR", instrument: "Bateria", img: victorPic },
-  {
-    name: "THAÍS",
-    instrument: "Teclado",
-    img: thaisPic,
-  },
-];
+import bandPicture from "../../assets/polaroid.png";
 
 export default function Members() {
   return (
     <section id="members">
       <SectionTitle>A Banda</SectionTitle>
 
-      <div className="members-grid">
-        {members.map((member, index) => (
-          <MemberCard
-            key={index}
-            name={member.name}
-            instrument={member.instrument}
-            img={member.img}
-          />
-        ))}
+      <div className="band-container">
+        <div className="band-info">
+          <p className="band-text">
+            Fundada no final de 2023 em Quixadá, a Ledo Engano é uma banda de
+            rock autoral que une influências de post-punk, grunge e hardcore.
+            Formado por Flávio, Adnny e Thais, o grupo foca no fortalecimento da
+            presença feminina e em uma identidade visual e temática inspirada no
+            cinema de terror e na moda punk. O objetivo central da banda é
+            expandir seu alcance regional através de composições próprias que
+            equilibram energia, melancolia e crítica social.
+          </p>
+        </div>
+
+        <div className="band-image-wrapper">
+          <img src={bandPicture} alt="Foto da Banda" className="band-photo" />
+          <div className="band-image-border"></div>
+        </div>
       </div>
     </section>
   );
